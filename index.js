8205055a2ba87e8be7a25b207ab31631866f24a8
Tara Chand Kumawat <tarachand122333@gmail.com>
2022-07-14 00:07:37 +0530

    fix google data display error

--- index.js
+++ index.js
@@ -1,45 +1,68 @@
 const express = require('express');
 const app = express();
-const passport = require('passport');
-const cookieSession = require('cookie-session');
-require('./passport');
-  
-app.use(cookieSession({
-    name: 'google-auth-session',
-    keys: ['key1', 'key2']
+const session = require('express-session');
+
+app.set('view engine', 'ejs');
+
+app.use(session({
+  resave: false,
+  saveUninitialized: true,
+  secret: 'SECRET' 
 }));
+
+app.get('/', function(req, res) {
+  res.render('pages/auth');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port , () => console.log('App listening on port ' + port));
+
+
+
+const passport = require('passport');
+var userProfile;
+
 app.use(passport.initialize());
 app.use(passport.session());
-    
-  
-app.get('/', (req, res) => {
-    res.send("<button><a href='/auth'>Login With Google</a></button>")
+
+app.set('view engine', 'ejs');
+
+app.get('/success', (req, res) => res.send(userProfile));
+app.get('/error', (req, res) => res.send("error logging in"));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
 });
-  
-// Auth 
-app.get('/auth' , passport.authenticate('google', { scope:
-    [ 'email', 'profile' ]
-}));
-  
-// Auth Callback
-app.get( '/auth/callback',
-    passport.authenticate( 'google', {
-        successRedirect: '/auth/callback/success',
-        failureRedirect: '/auth/callback/failure'
-}));
-  
-// Success 
-app.get('/auth/callback/success' , (req , res) => {
-    if(!req.user)
-        res.redirect('/auth/callback/failure');
-    res.send("Welcome " + req.user.email);
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
 });
-  
-// failure
-app.get('/auth/callback/failure' , (req , res) => {
-    res.send("Error");
-})
-  
-app.listen(4000 , () => {
-    console.log("Server Running on port 4000");
-});
+
+
+
+
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const GOOGLE_CLIENT_ID = '507199952697-ie442gh9u55puid9a41de8ema0q2b7b7.apps.googleusercontent.com';
+const GOOGLE_CLIENT_SECRET = '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';
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+      userProfile=profile;
+      return done(null, userProfile);
+  }
+));
+ 
+app.get('/auth/google', 
+  passport.authenticate('google', { scope : ['profile', 'email'] }));
+ 
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/error' }),
+  function(req, res) {
+    // Successful authentication, redirect success.
+    res.redirect('/success');
+  });
+
+
